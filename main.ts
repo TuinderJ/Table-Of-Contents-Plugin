5b80f797f53c5970f0ae02165daed4206b6d9da1
Joshua Tuinder <joshua.tuinder@gmail.com>
2024-05-04 09:23:31 -0600

    created plugin

--- main.ts
+++ main.ts
@@ -17,65 +17,22 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Table of Contents', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'create-tables-of-contents',
+			name: 'Create Tables of Contents',
+			callback: createTablesOfContents
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
+		
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -132,3 +89,90 @@ class SampleSettingTab extends PluginSettingTab {
 				}));
 	}
 }
+
+const DIRECTORY = 'Directory';
+const FILE = 'File'; 
+
+const createTablesOfContents = async () => {
+	const fs = require("fs/promises");
+	const dir = { name: 'root', directories: [], files: [] };
+	const basePath = app.vault.adapter.basePath;
+	const files = await fs.readdir(basePath);
+	for (const file of files) {
+		if (file.includes(".obsidian"))
+			continue;
+		if (file === 'Attachments')
+			continue;
+		if (file === '1.Table of Contents.md')
+			continue;
+		const fileType = await checkFile(fs, basePath, file);
+		switch (fileType) {
+			case DIRECTORY:
+				const subDir = await checkDir(fs, basePath, file)
+			dir.directories.push(subDir);
+			break;
+			case FILE:
+				dir.files.push(file);
+			break;
+		};
+	};
+	await makeTableOfContentsFile(fs, basePath, dir);
+};
+
+const checkDir = async (fs, basePath, dirPath) => {
+	const name = dirPath.split('/')[dirPath.split('/').length - 1];
+	const dir = { name, directories: [], files: [] };
+	const files = await fs.readdir(`${basePath}/${dirPath}`);
+	for (const file of files) {
+		if (file.includes(".obsidian"))
+			continue;
+		if (file === 'Attachments')
+			continue;
+		if (file === '1.Table of Contents.md')
+			continue;
+		const fileType = await checkFile(fs, basePath, `${dirPath}/${file}`);
+		switch (fileType) {
+			case DIRECTORY:
+				const subDir = await checkDir(fs, basePath, `${dirPath}/${file}`);
+			dir.directories.push(subDir);
+			break;
+			case FILE:
+				dir.files.push(file);
+			break;
+		};
+	};
+	await makeTableOfContentsFile(fs, `${basePath}/${dirPath}`, dir);
+	return dir;
+};
+
+const checkFile = async (fs, basePath, filePath) => {
+	let fileType = '';
+	const stats = await fs.stat(`${basePath}/${filePath}`);
+	if (stats.isDirectory()) fileType = DIRECTORY;
+	if (stats.isFile()) fileType = FILE;
+	return fileType;
+};
+
+const makeTableOfContentsFile = async (fs, filePath, dir) => {
+	let data = '# Folders\n';
+	dir.directories.forEach(directory => {
+		data += `- [[${directory.name}/1.Table of Contents|${directory.name}]]\n`;
+		data += makeSubContents(directory);
+	});
+	data += '# Files\n';
+	dir.files.forEach(file => {
+		data += `- [[${file.split('.md')[0]}]]\n`;
+	});
+	data = data.split('\n');
+	data.pop()
+	data = data.join('\n');
+	await fs.writeFile(`${filePath}/1.Table of Contents.md`, data);
+}
+
+const makeSubContents = (dir) => {
+	let data = '';
+	dir.files.forEach(file => {
+		data += `\t-[[${dir.name}/${file.split('.md')[0]}|${file.split('.md')[0]}]]\n`;
+	});
+	return data;
+};
